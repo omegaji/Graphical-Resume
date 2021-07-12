7fcb386aee1b8d7d466d5ac988fcfce3a115134f
om purohit <omegaji@pop-os.localdomain>
2021-07-13 01:34:04 +0530

    added ribbons

--- src/components/Left.js
+++ src/components/Left.js
@@ -9,7 +9,37 @@ export const Left=(props)=>{
         <div className="Left">
             <SearchSkill nodes={props.nodes}/>
             <SearchWork nodes={props.nodes}/>
+            <div className="BottomLeft">
+            <div className="ribbon">
+                <div className="rect" > <p className="rotate">&#x1F50D;</p><p className="rotate">Search</p> </div>
+                <div className="ribbottom">
+                <div className="arrow-down"></div>
+                <div className="arrow-down2"></div>
+            </div>
+            </div>
 
+            <div className="ribbon">
+                <div className="rect"><p className="rotate">&#x1F30D;</p><p className="rotate">Explore</p> </div>
+                <div className="ribbottom">
+                <div className="arrow-down"></div>
+                <div className="arrow-down2"></div>
+            </div>
+            </div>
+
+            <div className="ribbon">
+                <div className="rect"><p className="rotate">&#x1F447;</p><p className="rotate">Click</p> </div>
+                <div className="ribbottom">
+                <div className="arrow-down"></div>
+                <div className="arrow-down2"></div>
+            </div>
+            </div>
+
+                    
+
+                {/* <div></div>
+                <div>explorable</div>
+                <div>clickable</div> */}
+            </div>
         </div>
     )
 }
